495320cb53b49c77699ce0984e6b09c85f0f09e8
Panachai <panachai.ny@gmail.com>
2022-06-19 11:47:47 +0700

    apply Playground part1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Playground from "./components/Playground";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <h1>This is example how to use hook</h1>
+        <Playground />
+      </body>
     </div>
   );
 }
